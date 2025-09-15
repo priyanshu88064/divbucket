aeed873d72e7685bf098629c662a9a80fdf48e51
Priyanshu Tiwari <priyanshutiwari88064@gmail.com>
2025-09-15 10:39:10 +0530

    feat: vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import Playwrap from "./components/Playwrap/Playwrap";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
       <Playwrap />
+      <Analytics/>
     </Provider>
   </StrictMode>,
 );
