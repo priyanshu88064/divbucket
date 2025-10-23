1309a89a322fd0914c9a64096af595508ea49653
Priyanshu Tiwari <priyanshutiwari88064@gmail.com>
2025-10-23 10:53:33 +0530

    feat: wip

--- src/utils/Video/Video.tsx
+++ src/utils/Video/Video.tsx
@@ -1,11 +1,10 @@
 import { FaVideo } from "react-icons/fa";
 import Resizable from "../Resizable/Resizable";
-import styles from "./video.module.css";
 
 export default ({ node }: { node: number }) => {
   return (
     <Resizable id={node}>
-      <div className={styles.video}>
+      <div>
         <FaVideo size={40} color="gray" />
       </div>
     </Resizable>
