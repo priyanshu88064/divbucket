7b49114ca200b2160096fda7ef82c8e6289e696d
Priyanshu Tiwari <priyanshutiwari88064@gmail.com>
2025-10-25 19:28:33 +0530

    feat: improved drag-n-drop

--- src/hooks/useDrag.ts
+++ src/hooks/useDrag.ts
@@ -5,13 +5,7 @@ import type { AppDispatch } from "../store/store";
 import { createTemplate } from "../utils/template";
 import { moveItem } from "../store/reducers/treeReducer";
 
-const getPosi = (
-  x: number,
-  y: number,
-  rect: DOMRect,
-  display: string,
-  direction: string,
-) => {
+const getPosi = (x: number, y: number, rect: DOMRect) => {
   const xoff = x - rect.x;
   const yoff = y - rect.y;
   let xpos = 0,
@@ -21,25 +15,36 @@ const getPosi = (
   if (yoff > (rect.height * 20) / 100) ypos = 1;
   if (yoff > (rect.height * 80) / 100) ypos = 2;
 
-  if (display === "block" || ["column", "column-reverse"].includes(direction)) {
-    if (ypos === 0) return "top";
-    if (ypos === 1) return "inside";
-    return "bottom";
-  } else {
-    if (xpos === 0) return "left";
-    if (xpos === 1) return "inside";
-    return "right";
-  }
+  if (xpos === 1 && ypos === 1) return "inside";
+  if (ypos === 2) return "bottom";
+  if (xpos === 2) return "right";
+  if (ypos === 0) return "top";
+  return "left";
 };
 
-export function useDrag({ root }: { root: number }) {
+export function useDrag() {
   const dispatch = useDispatch<AppDispatch>();
   const draggedNodeRef = useRef<HTMLDivElement | null>(null);
   const draggedWrapperRef = useRef<HTMLDivElement | null>(null);
+  const overlayRef = useRef<HTMLDivElement | null>(null);
+  const posRef = useRef(-1); // [0] - add before, [1] - add after, [-1] - add inside
+
+  const initOverlay = () => {
+    let overlay = document.createElement("div");
+    overlay.classList.add(
+      "fixed",
+      "bg-hoverblue/60",
+      "rounded-md",
+      "pointer-events-none",
+      "transition-all",
+      "ease-out",
+    );
+    document.body.appendChild(overlay);
+    overlayRef.current = overlay;
+  };
 
   const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
     e.stopPropagation();
-    if (!(e.target as HTMLDivElement).getAttribute("data-id")) return;
     draggedNodeRef.current = e.target as HTMLDivElement;
 
     // dummy div for creating drag image
@@ -56,161 +61,115 @@ export function useDrag({ root }: { root: number }) {
     draggedWrapperRef.current = draggedWrapper;
   };
 
-  const handleDragEnd = () => {
-    draggedNodeRef.current = null;
-    if (draggedWrapperRef && draggedWrapperRef.current)
-      draggedWrapperRef.current.remove();
-    draggedWrapperRef.current = null;
-  };
-
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
 
+    if (!overlayRef.current) initOverlay();
     const targetId = (e.target as HTMLDivElement).getAttribute("data-id");
+    const targetType = (e.target as HTMLDivElement).getAttribute("data-type");
     const draggedId = draggedNodeRef.current?.getAttribute("data-id");
 
     if (
       !targetId ||
-      Number(targetId) === root ||
-      !draggedId ||
-      draggedId === targetId
+      draggedId === targetId ||
+      !overlayRef.current ||
+      !targetType ||
+      !["root", "Row", "Block"].includes(targetType)
     )
       return;
 
-    const display = (e.target as HTMLDivElement).style.display || "block";
-    const direction = (e.target as HTMLDivElement).style.flexDirection || "row";
-
-    (e.target as HTMLDivElement).classList.add(styles.dborder);
     const rect = (e.target as HTMLDivElement).getBoundingClientRect();
-    // switch (getPosi(e.clientX, e.clientY, rect, display, direction)) {
-    //   case "inside":
-    //     (e.target as HTMLDivElement).classList.remove(
-    //       styles.dtop,
-    //       styles.dbottom,
-    //       styles.dleft,
-    //       styles.dright,
-    //     );
-    //     (e.target as HTMLDivElement).classList.add(styles.dinside);
-    //     break;
-    //   case "top":
-    //     (e.target as HTMLDivElement).classList.remove(
-    //       styles.dinside,
-    //       styles.dbottom,
-    //       styles.dleft,
-    //       styles.dright,
-    //     );
-    //     (e.target as HTMLDivElement).classList.add(styles.dtop);
-    //     break;
-    //   case "right":
-    //     (e.target as HTMLDivElement).classList.remove(
-    //       styles.dinside,
-    //       styles.dtop,
-    //       styles.dbottom,
-    //       styles.dleft,
-    //     );
-    //     (e.target as HTMLDivElement).classList.add(styles.dright);
-    //     break;
-    //   case "bottom":
-    //     (e.target as HTMLDivElement).classList.remove(
-    //       styles.dinside,
-    //       styles.dtop,
-    //       styles.dleft,
-    //       styles.dright,
-    //     );
-    //     (e.target as HTMLDivElement).classList.add(styles.dbottom);
-    //     break;
-    //   case "left":
-    //     (e.target as HTMLDivElement).classList.remove(
-    //       styles.dinside,
-    //       styles.dtop,
-    //       styles.dbottom,
-    //       styles.dright,
-    //     );
-    //     (e.target as HTMLDivElement).classList.add(styles.dleft);
-    //     break;
-    //   default:
-    //     break;
-    // }
+
+    if (targetType === "root") {
+      overlayRef.current.style.width = "0";
+      overlayRef.current.style.height = "0";
+      posRef.current = -1;
+
+      return;
+    }
+
+    switch (getPosi(e.clientX, e.clientY, rect)) {
+      case "inside":
+        overlayRef.current.style.top = rect.top + "px";
+        overlayRef.current.style.left = rect.left + "px";
+        overlayRef.current.style.width = rect.width + "px";
+        overlayRef.current.style.height = rect.height + "px";
+        posRef.current = -1;
+        break;
+      case "top":
+        overlayRef.current.style.top = rect.top + "px";
+        overlayRef.current.style.left = rect.left + "px";
+        overlayRef.current.style.width = rect.width + "px";
+        overlayRef.current.style.height = "4px";
+        posRef.current = 0;
+        break;
+      case "right":
+        overlayRef.current.style.top = rect.top + "px";
+        overlayRef.current.style.left = rect.left + rect.width + "px";
+        overlayRef.current.style.width = "4px";
+        overlayRef.current.style.height = rect.height + "px";
+        posRef.current = 1;
+        break;
+      case "bottom":
+        overlayRef.current.style.top = rect.top + rect.height + "px";
+        overlayRef.current.style.left = rect.left + "px";
+        overlayRef.current.style.width = rect.width + "px";
+        overlayRef.current.style.height = "4px";
+        posRef.current = 1;
+        break;
+      case "left":
+        overlayRef.current.style.top = rect.top + "px";
+        overlayRef.current.style.left = rect.left + "px";
+        overlayRef.current.style.width = "4px";
+        overlayRef.current.style.height = rect.height + "px";
+        posRef.current = 0;
+        break;
+    }
   };
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
     e.stopPropagation();
+    if (!draggedNodeRef.current) return;
 
-    (e.target as HTMLDivElement).classList.remove(
-      styles.dborder,
-      styles.dinside,
-      styles.dtop,
-      styles.dbottom,
-      styles.dleft,
-      styles.dright,
+    const targetId = (e.target as HTMLDivElement).getAttribute("data-id");
+    const draggedId =
+      draggedNodeRef.current.getAttribute("data-id") ||
+      createTemplate({
+        type: draggedNodeRef.current.getAttribute("data-type") as string,
+        dispatch,
+      });
+
+    if (targetId === draggedId) return;
+
+    dispatch(
+      moveItem({
+        node: draggedId,
+        referenceNode: targetId,
+        pos: posRef.current,
+      }),
     );
 
-    const targetId =
-      (e.target as HTMLDivElement).getAttribute("data-target") ||
-      (e.target as HTMLDivElement).getAttribute("data-root");
-    const draggedId =
-      draggedNodeRef.current?.getAttribute("data-id") ||
-      createTemplate({ type: e.dataTransfer.getData("type"), dispatch });
+    cleanup();
+  };
 
-    if (!targetId || !draggedId || draggedId == targetId) return;
+  const handleDragLeave = () => {};
 
-    if (Number(targetId) === root) {
-      dispatch(moveItem({ node: draggedId, referenceNode: targetId, pos: -1 }));
-    } else {
-      let aParent = (e.target as HTMLDivElement).parentNode
-        ?.parentNode as HTMLDivElement;
-      if ((e.target as HTMLDivElement).getAttribute("data-target"))
-        aParent = aParent?.parentNode as HTMLDivElement;
-      if (!aParent) return;
-      const display = aParent.style.display || "block";
-      const direction = aParent.style.flexDirection || "row";
+  const handleDragEnd = () => {
+    cleanup();
+  };
 
-      const rect = (e.target as HTMLDivElement).getBoundingClientRect();
-      switch (getPosi(e.clientX, e.clientY, rect, display, direction)) {
-        case "inside":
-          dispatch(
-            moveItem({ node: draggedId, referenceNode: targetId, pos: -1 }),
-          );
-          break;
-        case "top":
-          dispatch(
-            moveItem({ node: draggedId, referenceNode: targetId, pos: 0 }),
-          );
-          break;
-        case "right":
-          dispatch(
-            moveItem({ node: draggedId, referenceNode: targetId, pos: 1 }),
-          );
-          break;
-        case "bottom":
-          dispatch(
-            moveItem({ node: draggedId, referenceNode: targetId, pos: 1 }),
-          );
-          break;
-        case "left":
-          dispatch(
-            moveItem({ node: draggedId, referenceNode: targetId, pos: 0 }),
-          );
-          break;
-        default:
-          break;
-      }
-    }
+  const cleanup = () => {
     draggedNodeRef.current = null;
     if (draggedWrapperRef && draggedWrapperRef.current)
       draggedWrapperRef.current.remove();
     draggedWrapperRef.current = null;
-  };
-  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
-    (e.target as HTMLDivElement).classList.remove(
-      styles.dborder,
-      styles.dinside,
-      styles.dtop,
-      styles.dbottom,
-      styles.dleft,
-      styles.dright,
-    );
+    if (overlayRef.current) {
+      document.body.removeChild(overlayRef.current);
+      overlayRef.current = null;
+    }
+    posRef.current = -1;
   };
 
   return {
--- src/utils/Resizable/Resizable.tsx
+++ src/utils/Resizable/Resizable.tsx
@@ -1,15 +1,9 @@
-import styles from "./resizable.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import {
   updateActiveNode,
   updateHoverNodeId,
 } from "../../store/reducers/treeReducer";
 import ContextMenu from "../../components/ContextMenu/ContextMenu";
-import { changeTab } from "../../store/reducers/focusReducer";
-import { MdOutlineEdit } from "react-icons/md";
-import { TbMinusVertical } from "react-icons/tb";
-import { GetIconOfType } from "../../components/Cssbar/Cssbar";
-import { RiDragMove2Fill } from "react-icons/ri";
 import type { AppDispatch, RootState } from "../../store/store";
 import React from "react";
 import { useResizer } from "../../hooks/useResizer";
@@ -32,38 +26,43 @@ export default function Resizable({
   const { dim, divRef, handleMouseDown } = useResizer({ id });
   const { clicked, setClicked, points, setPoints } = useContextMenu();
 
-  const pureNode = (
-    <div
-      ref={divRef}
-      id={type === "root" ? `node-root` : `node-${id}`}
-      // data-root={id}
-      // data-target={id}
-      data-id={id}
-      style={{ ...styleMap, ...dim }}
-      onClick={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        dispatch(updateActiveNode({ id }));
-      }}
-      onContextMenu={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setClicked(true);
-        setPoints({ x: e.pageX, y: e.pageY });
-        dispatch(updateActiveNode({ id }));
-      }}
-      onMouseOver={(e) => {
-        e.stopPropagation();
-        dispatch(updateHoverNodeId({ id }));
-      }}
-      onMouseLeave={(e) => {
-        e.stopPropagation();
-        dispatch(updateHoverNodeId({ id: null }));
-      }}
-    >
-      {children}
-    </div>
-  );
+  const pureNode = (() => {
+    // because not every element can be a div (eg. video, image)
+    switch (type) {
+      default:
+        return (
+          <div
+            ref={divRef}
+            id={type === "root" ? `node-root` : `node-${id}`}
+            data-id={id}
+            data-type={type}
+            style={{ ...styleMap, ...dim }}
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              dispatch(updateActiveNode({ id }));
+            }}
+            onContextMenu={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              setClicked(true);
+              setPoints({ x: e.pageX, y: e.pageY });
+              dispatch(updateActiveNode({ id }));
+            }}
+            onMouseOver={(e) => {
+              e.stopPropagation();
+              dispatch(updateHoverNodeId({ id }));
+            }}
+            onMouseLeave={(e) => {
+              e.stopPropagation();
+              dispatch(updateHoverNodeId({ id: null }));
+            }}
+          >
+            {children}
+          </div>
+        );
+    }
+  })();
 
   if (type === "root") {
     return (
@@ -95,154 +94,3 @@ export default function Resizable({
     </>
   );
 }
-
-export function Resizable2({
-  id,
-  children,
-}: {
-  id: number;
-  children: React.ReactNode;
-}) {
-  const dispatch = useDispatch<AppDispatch>();
-  const activeNodeId = useSelector(
-    (state: RootState) => state.treeReducer.activeNodeId,
-  );
-  const styleMap = useSelector(
-    (state: RootState) => state.treeReducer.styleMap[id].default,
-  );
-  const name = useSelector(
-    (state: RootState) => state.treeReducer.dataMap[id].name,
-  );
-  const type = useSelector(
-    (state: RootState) => state.treeReducer.dataMap[id].type,
-  );
-  const unit = useSelector(
-    (state: RootState) => state.treeReducer.dataMap[id].unit,
-  );
-  const { dim, divRef, handleMouseDown } = useResizer({ id });
-  const { clicked, setClicked, points, setPoints } = useContextMenu();
-
-  return (
-    <div className={`${styles.awrap} ${type === "root" && styles.root}`}>
-      {clicked && (
-        <ContextMenu id={id} points={points} setClicked={setClicked} />
-      )}
-      <div
-        ref={divRef}
-        data-root={id}
-        className={`${styles.a}`}
-        style={{ ...styleMap, ...dim }}
-        onClick={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          if (id !== activeNodeId) dispatch(updateActiveNode({ id }));
-        }}
-        onContextMenu={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          setClicked(true);
-          setPoints({ x: e.pageX, y: e.pageY });
-          dispatch(updateActiveNode({ id }));
-        }}
-      >
-        {id === activeNodeId ? (
-          <>
-            <InfoBar name={name} type={type} id={id} />
-            {type !== "root" && (
-              <>
-                <div
-                  draggable={false}
-                  onMouseDown={(e) => handleMouseDown(e, 0)}
-                  className={`${styles.resizable} ${styles.top}`}
-                ></div>
-                <div
-                  draggable={false}
-                  onMouseDown={(e) => handleMouseDown(e, 1)}
-                  className={`${styles.resizable} ${styles.right}`}
-                ></div>
-                <div
-                  draggable={false}
-                  onMouseDown={(e) => handleMouseDown(e, 2)}
-                  className={`${styles.resizable} ${styles.bottom}`}
-                ></div>
-                <div
-                  draggable={false}
-                  onMouseDown={(e) => handleMouseDown(e, 3)}
-                  className={`${styles.resizable} ${styles.left}`}
-                ></div>
-                <div
-                  onMouseDown={(e) => handleMouseDown(e, 0)}
-                  className={`${styles.circle} ${styles.ctop}`}
-                ></div>
-                <div
-                  onMouseDown={(e) => handleMouseDown(e, 1)}
-                  className={`${styles.circle} ${styles.cright}`}
-                ></div>
-                <div
-                  onMouseDown={(e) => handleMouseDown(e, 2)}
-                  className={`${styles.circle} ${styles.cbottom}`}
-                ></div>
-                <div
-                  onMouseDown={(e) => handleMouseDown(e, 3)}
-                  className={`${styles.circle} ${styles.cleft}`}
-                ></div>
-              </>
-            )}
-          </>
-        ) : (
-          type !== "root" && (
-            <div
-              data-target={id}
-              className={`${styles.hov} ${unit && styles.unit}`}
-            ></div>
-          )
-        )}
-        {children}
-      </div>
-      {type === "root" && (
-        <div
-          onMouseDown={(e) => handleMouseDown(e, 1)}
-          className={`${styles.resizablebar} ${styles.rightbar}`}
-        >
-          <TbMinusVertical className={styles.lines} />
-        </div>
-      )}
-    </div>
-  );
-}
-
-const InfoBar = ({
-  name,
-  type,
-  id,
-}: {
-  name: string;
-  type: string;
-  id: number;
-}) => {
-  const dispatch = useDispatch();
-  return (
-    <div className="flex absolute top-0 left-0 -translate-y-full text-white bg-[#ff0099] rounded-xs z-[2] font-normal">
-      <div className="text-xs flex items-center justify-center px-[7px] py-[5px] gap-[5px] border-r border-[rgba(255, 255, 255, 0.285)]">
-        {GetIconOfType(type, 12)}
-        {name}
-      </div>
-      <div
-        style={{ cursor: "pointer" }}
-        title="edit"
-        onClick={() => dispatch(changeTab({ tab: "11" }))}
-      >
-        <MdOutlineEdit size={12} />
-      </div>
-      {type !== "root" && (
-        <div
-          draggable
-          data-id={id}
-          style={{ borderRight: "none", cursor: "grab" }}
-        >
-          <RiDragMove2Fill size={14} />
-        </div>
-      )}
-    </div>
-  );
-};
