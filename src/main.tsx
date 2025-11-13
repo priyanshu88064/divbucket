5541e1742e7df375ff875ef286dbff87233cb851
Priyanshu Tiwari <priyanshutiwari88064@gmail.com>
2025-11-13 20:44:31 +0530

    feat: loader

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import Playwrap from "./components/App/App";
+import App from "./components/App/App";
 import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
-      <Playwrap />
+      <App />
       <Analytics />
     </Provider>
   </StrictMode>,
