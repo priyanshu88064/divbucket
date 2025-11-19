c9d9ce3d75bc44f4dd5fec3bcea8841ad020d9d1
Priyanshu Tiwari <priyanshutiwari88064@gmail.com>
2025-11-19 14:04:22 +0530

    fix: temporary position fix

---
+++ src/hooks/syncCSSPosition.ts
@@ -0,0 +1,53 @@
+import { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import type { RootState } from "../store/store";
+
+export function syncCSSPosition() {
+  const tree = useSelector((state: RootState) => state.treeReducer.tree);
+  const rootNodePosition = useRef<DOMRect | null>(
+    document.getElementById("node-root")
+      ? (
+          document.getElementById("node-root") as HTMLDivElement
+        ).getBoundingClientRect()
+      : null,
+  );
+
+  useEffect(() => {
+    const update = () => {
+      rootNodePosition.current = (
+        document.getElementById("node-root") as HTMLDivElement
+      ).getBoundingClientRect();
+
+      const syncEntireTree = (id: number) => {
+        syncPositionById(id);
+        tree[id].map((child) => syncEntireTree(child));
+      };
+      tree[-1].map((child) => syncEntireTree(child));
+    };
+
+    const rootNode = document.getElementById("node-root") as HTMLDivElement;
+    const playground = document.getElementById("playground");
+    if (!rootNode || !playground) return;
+
+    const observer = new ResizeObserver(update);
+    observer.observe(rootNode);
+
+    const observer2 = new ResizeObserver(update);
+    observer2.observe(playground);
+
+    return () => {
+      observer.disconnect();
+      observer2.disconnect();
+    };
+  }, []);
+
+  const syncPositionById = (id: number) => {
+    let node = document.getElementById("node-" + id);
+    if (!node || !rootNodePosition.current) return;
+
+    if (node.style.position === "fixed") {
+      node.style.transform = `translate(${Math.floor(rootNodePosition.current.left)}px, ${Math.floor(rootNodePosition.current.top)}px)`;
+      node.style.width = `${Math.floor(rootNodePosition.current.width)}px`;
+    }
+  };
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
-import { StrictMode } from "react";
+import { lazy, StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import App from "./components/App/App";
 import { Analytics } from "@vercel/analytics/react";
+import Loader from "./components/Overlays/Loader/Loader";
+
+const App = lazy(() => import("./components/App/App"));
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Suspense fallback={<Loader />}>
+        <App />
+      </Suspense>
       <Analytics />
     </Provider>
   </StrictMode>,
